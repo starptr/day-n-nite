87a5c3f12c46ad0a58a1866f43a7c7f4ba4a83e7
Yuto Nishida <starptr@users.noreply.github.com>
2021-06-10 18:54:00 -0700

    Add wezterm

---
+++ src/themer/wezterm.rs
@@ -0,0 +1,14 @@
+use std::fs;
+use dirs;
+use super::SetError;
+
+// Force WezTerm to reload config to read new color mode
+pub fn set() -> Result<(), SetError> {
+    let mut wezterm_config_file = dirs::home_dir().unwrap();
+    wezterm_config_file.push(".config");
+    wezterm_config_file.push("wezterm");
+    wezterm_config_file.push("wezterm.lua");
+    let config = fs::read(&wezterm_config_file).map_err(|_| SetError::ReadFailure)?;
+    fs::write(wezterm_config_file, config).map_err(|_| SetError::WriteFailure)?;
+    Ok(())
+}
