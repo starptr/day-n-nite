bf03370b037085f9a7b111b74920c3efd4662af4
Yuto Nishida <yuyu.nishida@gmail.com>
2021-06-17 13:04:11 -0700

    Change env macro to option_env to avoid panics

--- src/themer/emit.rs
+++ src/themer/emit.rs
@@ -2,7 +2,7 @@ use super::{Mode, ModuleError, SetError};
 use std::process::Command;
 
 pub fn set(mode: Mode) -> Result<(), ModuleError> {
-    if env!("IS_WSL") == "true" {
+    if option_env!("IS_WSL").unwrap_or("false") == "true" {
         Command::new("powershell.exe")
             .args(&["-C", "day-n-nite", "--no-emit"])
             .output()
--- src/themer/system_os.rs
+++ src/themer/system_os.rs
@@ -8,7 +8,7 @@ use std::env::consts::OS;
 // Should be called on every system
 // OS conditional is done in the function
 pub fn set(mode: Mode) -> Result<(), ModuleError> {
-    if (env!("IS_WSL") == "true") || OS == "windows" {
+    if option_env!("IS_WSL").unwrap_or("false") == "true" || OS == "windows" {
         Command::new("reg.exe")
             .args(&["add", r"HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "/v", "AppsUseLightTheme", "/t", "REG_DWORD", "/d", if mode == Mode::Day {"1"} else {"0"}, "/f"]).output().map_err(|_| ModuleError::SystemWindows(SetError::CommandFailure))?;
         Command::new("reg.exe")
