4878a8ad8483441f40674795de8ad225f12d91b4
Yuto Nishida <yuyu.nishida@gmail.com>
2021-06-17 18:24:52 -0700

    Use cargo crate env vars for clap metadata

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,10 @@
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg};
 
 pub fn get_app() -> App<'static, 'static> {
-    App::new("day-n-nite")
-        .version("0.1.0")
-        .author("Yuto Nishida")
-        .about("Global light/dark mode toggler")
+    App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("no_emit")
                 .help("Whether to emit calls to other systems")
