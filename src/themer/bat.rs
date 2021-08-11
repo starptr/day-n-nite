a382970cdfab6fda5d27f238c82befa6ca451a60
Yuto Nishida <starptr@users.noreply.github.com>
2021-08-11 12:45:52 -0700

    Use path joins

--- src/themer/bat.rs
+++ src/themer/bat.rs
@@ -5,10 +5,7 @@ use super::Mode;
 
 // Use simple string replacer to convert day-n-nite template to readable config
 pub fn set(mode: Mode) -> Result<(), ModuleError> {
-    let mut bat_config_file = dirs::home_dir().unwrap();
-    bat_config_file.push(".config");
-    bat_config_file.push("bat");
-    bat_config_file.push("config.day-n-nite");
+    let bat_config_file = dirs::home_dir().unwrap().join(".config").join("bat").join("config.day-n-nite");
     let mut bat_config_generated_file = bat_config_file.clone();
     bat_config_generated_file.set_extension("");
     simple_string_replace(mode, bat_config_file, bat_config_generated_file).map_err(|e| ModuleError::Bat(e))?;
--- src/themer/wezterm.rs
+++ src/themer/wezterm.rs
@@ -5,10 +5,7 @@ use super::ModuleError;
 
 // Force WezTerm to reload config to read new color mode
 pub fn update() -> Result<(), ModuleError> {
-    let mut wezterm_config_file = dirs::home_dir().unwrap();
-    wezterm_config_file.push(".config");
-    wezterm_config_file.push("wezterm");
-    wezterm_config_file.push("wezterm.lua");
+    let wezterm_config_file = dirs::home_dir().unwrap().join(".config").join("wezterm").join("wezterm.lua");
     let config = fs::read(&wezterm_config_file).map_err(|_| ModuleError::Wezterm(SetError::ReadFailure))?;
     fs::write(wezterm_config_file, config).map_err(|_| ModuleError::Wezterm(SetError::WriteFailure))?;
     Ok(())
