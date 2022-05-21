18720962c2806a55dee50d554d27b9852d168cff
Yuto Nishida <yuto@berkeley.edu>
2022-05-21 16:21:05 -0700

    Reset project

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-mod cli;
-mod themer;
-use std::string::ToString;
-
 fn main() {
-    let arg_matches = cli::get_app().get_matches();
-
-    let target_mode = if arg_matches.is_present("daymode") {
-        Ok(themer::Mode::Day)
-    } else if arg_matches.is_present("nightmode") {
-        Ok(themer::Mode::Night)
-    } else {
-        themer::get_mode().map(|mode| themer::toggle(mode))
-    };
-
-    let set_result = match target_mode {
-        Ok(mode) => themer::set_mode(mode, arg_matches),
-        Err(themer::GetError::NoMode) => themer::set_mode(themer::Mode::Day, arg_matches),
-        Err(themer::GetError::UnknownMode) => themer::set_mode(themer::Mode::Day, arg_matches),
-    };
-
-    match set_result {
-        Ok(mode) => {
-            println!("Set to {} Mode.", mode.to_string());
-        }
-        Err(e) => {
-            eprintln!("Error: {:?}", e);
-        }
-    };
+    println!("Hello, world!");
 }
